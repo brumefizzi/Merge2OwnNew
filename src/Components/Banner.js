5ab1a4bea786df60ab78b5e631dffbc8f243416a
Brume Ikogho <brumethedev@gmail.com>
2023-06-09 23:03:11 -0400

    modifications to banner and navbar

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -14,40 +14,34 @@ export const Banner = () => {
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <>
-                  <div style={{ position: 'relative' }}>
-                    <div className="desktop5-group206">
-                      <span className="desktop5-text04">
-                        <span className="desktop5-text05">
-                          <span>Co-ownership</span>
-                          <br />
-                          <span>for </span>
-                        </span>
-                        <span className="desktop5-text09">everybody!</span>
+              {({ isVisible }) => // even though isVisible is not used, it won't be passed down to the div
+                <div style={{ position: 'relative' }}>
+                  <div className="desktop5-group206">
+                    <div className="desktop5-text04">
+                      <span className="desktop5-text05">
+                        <span>Co-ownership</span>
+                        <br />
+                        <span>for </span>
                       </span>
-                      <img
-                        alt="Vector3136"
-                        src={require("../assets/img/vector3136-leq.svg")}
-                        className="desktop5-vector"
-                      />
+                      <span className="desktop5-text09">Everybody!</span>
                     </div>
+                    
                   </div>
-                  <div>
+                  <div style={{ position: 'relative', zIndex: 2, margin: '20px 0' }}>
                     <p>
                       Are you experiencing challenges in buying your own home or interested in providing services to enable people buy their own home?
                       If you are excited as we are in finding creative solutions, please join the waitlist!
                     </p>
                     <MailchimpForm />
                   </div>
-                </>
+                </div>
               }
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                <div>
                   <img src={headerImg} alt="Header Img" style={{ borderRadius: "25px" }}/>
                 </div>}
             </TrackVisibility>
--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from "react";
-import { Col} from "react-bootstrap";
+import { Col, Row, Alert } from "react-bootstrap";
 
 
 export const Newsletter = ({ status, message, onValidated }) => {
@@ -24,22 +24,25 @@ export const Newsletter = ({ status, message, onValidated }) => {
   }
 
   return (
-    <Col lg={12}>
-  <form onSubmit={handleSubmit}>
-    <div className="new-email-bx d-flex flex-column flex-md-row">
-      <input className="new-email-bx mb-3 mb-md-0 mr-md-2 flex-grow-1" value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email Address" style={{ flex: '1', marginRight: '2px' }} />
-      <button type="submit" style={{ flex: '0' }}>Submit</button>
-    </div>
-  </form>
-</Col>
-
-
-
-
-
-
-
-
-  
+      <Col lg={12}>
+        <div>
+          <Row>
+            <Col lg={12} md={6} xl={5}>
+              <h3>Join the waitlist<br></br> & never miss latest updates</h3>
+              {status === 'sending' && <Alert>Sending...</Alert>}
+              {status === 'error' && <Alert variant="danger">{message}</Alert>}
+              {status === 'success' && <Alert variant="success">{message}</Alert>}
+            </Col>
+            <Col md={6} xl={7}>
+              <form onSubmit={handleSubmit}>
+                <div className="new-email-bx d-flex flex-column flex-md-row">
+                  <input className=" new-email-bx mb-3 mb-md-0 mr-md-2 flex-grow-1" value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email Address" />
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            </Col>
+          </Row>
+        </div>
+      </Col>
   )
 }
